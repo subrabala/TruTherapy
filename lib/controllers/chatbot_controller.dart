9bdab096dc971748773df5ebc55c37ef7589d0fc
sub-dcx <subrabala.dash@coindcx.com>
2024-10-17 01:19:58 +0530

    Q and A sorted

--- lib/controllers/chatbot_controller.dart
+++ lib/controllers/chatbot_controller.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
+import 'package:fsui/constants.dart'; // Ensure AppColors is imported
 
 class ChatBotController extends GetxController {
   var question = 'How are you feeling?'.obs;
   var answers = <Map<String, String>>[].obs;
+  var selectedAnswers = <String>[].obs;
 
   @override
   void onInit() {
@@ -20,7 +22,23 @@ class ChatBotController extends GetxController {
   }
 
   void selectAnswer(String answerId) {
-    question.value =
-        'Tell me more about why you feel ${answers.firstWhere((a) => a['id'] == answerId)['text']}.';
+    selectedAnswers.add(answers.firstWhere((a) => a['id'] == answerId)['text']!);
+    fetchNewQuestionAndAnswers(answerId);
+  }
+
+  void fetchNewQuestionAndAnswers(String answerId) {
+    if (answerId == '1') {
+      question.value = 'Why do you feel Not OK?';
+      answers.value = [
+        {'id': '5', 'text': 'I am feeling tired'},
+        {'id': '6', 'text': 'I have a headache'},
+      ];
+    } else if (answerId == '2') {
+      question.value = 'What makes you feel OK?';
+      answers.value = [
+        {'id': '7', 'text': 'Just relaxing'},
+        {'id': '8', 'text': 'Talking to friends'},
+      ];
+    }
   }
 }
--- lib/screens/bottom_navbar_screens/chatbot_screen.dart
+++ lib/screens/bottom_navbar_screens/chatbot_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fsui/controllers/chatbot_controller.dart';
 import 'package:get/get.dart';
+import 'package:fsui/constants.dart'; // Ensure AppColors is imported
 
 class ChatBotScreen extends StatelessWidget {
   final ChatBotController controller = Get.put(ChatBotController());
@@ -10,37 +11,67 @@ class ChatBotScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chatbot'),
+        backgroundColor: AppColors.dark800,
       ),
-      body: Column(
-        children: [
-          Obx(() => Padding(
+      body: Obx(() {
+        return ListView.builder(
+          itemCount: controller.selectedAnswers.length + 1 + controller.answers.length,
+          itemBuilder: (context, index) {
+            // Display question
+            if (index == 0) {
+              return Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  controller.question.value,
-                  style: const TextStyle(fontSize: 18),
+                child: Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: AppColors.light100,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    controller.question.value,
+                    style: const TextStyle(fontSize: 18),
+                  ),
                 ),
-              )),
-          Expanded(
-            child: Obx(() {
-              return ListView.builder(
-                itemCount: controller.answers.length,
-                itemBuilder: (context, index) {
-                  final answer = controller.answers[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        controller.selectAnswer(answer['id']!);
-                      },
-                      child: Text(answer['text']!),
-                    ),
-                  );
-                },
               );
-            }),
-          ),
-        ],
-      ),
+            }
+
+            // Display selected answers
+            if (index <= controller.selectedAnswers.length) {
+              final answer = controller.selectedAnswers[index - 1];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: AppColors.light100,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    answer,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              );
+            }
+
+            // Display answer options
+            final answer = controller.answers[index - controller.selectedAnswers.length - 1];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  controller.selectAnswer(answer['id']!);
+                },
+                child: Text(answer['text']!),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: AppColors.dark800,
+                  backgroundColor: AppColors.light100,
+                ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
