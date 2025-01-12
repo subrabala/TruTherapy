1afca9a6a87f63f2a9a8fe4a2c2efbc3e18d2ad2
sub-dcx <subrabala.dash@coindcx.com>
2025-01-12 07:35:30 +0530

    no device info required

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 
 class SharedPrefs {
   static final SharedPrefs _instance = SharedPrefs._internal();
@@ -98,15 +97,3 @@ String? getScope() {
   return prefs.getString('scope');
 }
 
-
-Future<String?> getDeviceId() async {
-  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-  if (Platform.isAndroid) {
-    final androidInfo = await deviceInfo.androidInfo;
-    return androidInfo.id;
-  } else if (Platform.isIOS) {
-    final iosInfo = await deviceInfo.iosInfo;
-    return iosInfo.identifierForVendor;
-  }
-  return null;
-}
